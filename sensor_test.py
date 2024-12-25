c18505d88a574d4f4800dacd4864028daa10c326
kdiep12 <diepthanhkhang@gmail.com>
2024-12-25 15:50:04 -0800

    Change value

--- sensor_test.py
+++ sensor_test.py
@@ -7,10 +7,12 @@ water_sensor = InputDevice(SENSOR_PIN, pull_up=True) # pull-up resistor disabled
 
 try:
     while True:
-        if water_sensor.value == 0:
-            print("Water level LOW - Activating pump")  
+        if water_sensor.value == 1:
+            print("Water level LOW")
+            sleep(0.5)
         else:
-            print("Water level OK - Pump off")            
+            print("Water level OK")
+            sleep(0.5)
         sleep(1)
 except KeyboardInterrupt:
     print("Exiting program")
